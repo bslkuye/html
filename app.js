e615b5c0ee63ce26065d43fed05efd92090438e3
정도헌 <89259535+bslkuye@users.noreply.github.com>
2022-06-24 14:27:13 +0000

    js practice

--- app.js
+++ app.js
@@ -125,10 +125,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 // // h1.style.color = "blue";
 
 function handleTitleClick() {
-  if (h1.className === "clicked") {
+  const clickedClass = "clicked"
+  if (h1.className === clickedClass) {
     h1.className = "";
   }else{
-    h1.className = "clicked";
+    h1.className = clickedClass;
   }
 }
 
