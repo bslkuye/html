31ba83edd84b6665358e5e86de51665f176c4352
정도헌 <89259535+bslkuye@users.noreply.github.com>
2022-07-02 14:42:16 +0000

    js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 
+const link = document.querySelector("a");
+
 function onSubmit(event) {
   event.preventDefault();
   console.log(event);
 }
 
+function handleLinkClick() {
+  alert("clicked!");
+}
+
 
 loginForm.addEventListener("submit",onSubmit);
-
+link.addEventListener("click", handleLinkClick);
