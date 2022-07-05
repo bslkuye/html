2b8ad9dd2e2152f6280e03aaecf36b0ebf348143
정도헌 <89259535+bslkuye@users.noreply.github.com>
2022-07-05 13:07:30 +0000

    JS

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
-function onSubmit(event) {
-  event.preventDefault();
-  console.log(event);
-}
-
-function handleLinkClick() {
+function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 
-loginForm.addEventListener("submit",onSubmit);
-link.addEventListener("click", handleLinkClick);
+loginForm.addEventListener("submit",onLoginSubmit);
