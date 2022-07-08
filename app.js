f351776ae737828afd4154fbf7f1afe59bef05ad
정도헌 <89259535+bslkuye@users.noreply.github.com>
2022-07-08 14:56:44 +0000

    js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username"
 
-function paintGreeting(username) {
+function paintGreeting() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  // greeting.innerText = "Hello " + username;
-  paintGreeting(username);
+  paintGreeting();
 }
 
 
@@ -27,5 +27,5 @@ if(savedUsername == null){
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit",onLoginSubmit);
 }else{
-  paintGreeting(savedUsername);
+  paintGreeting();
 }
