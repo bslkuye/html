f2300d8676bcba00d03153308a4c046a49e4c67e
bslkuye <jdh960401@naver.com>
2022-10-16 22:16:48 +0900

    programmers

--- .vscode/fractal/main.js
+++ .vscode/fractal/main.js
@@ -6,21 +6,21 @@ const paintBtn = document.getElementById("paint");
 const oneMoveBtn = document.getElementById("oneMove");
 
 const COLORS = [
-    '#000000',
-    '#111111',
-    '#222222',
-    '#333333',
-    '#444444',
-    '#555555',
-    '#666666',
-    '#777777',
-    '#888888',
-    '#999999',
-    '#AAAAAA',
-    '#BBBBBB',
-    '#CCCCCC',
-    '#DDDDDD',
-    '#EEEEEE',
+    '#0000FF',
+    '#1111FF',
+    '#2222FF',
+    '#3333FF',
+    '#4444FF',
+    '#5555FF',
+    '#6666FF',
+    '#7777FF',
+    '#8888FF',
+    '#9999FF',
+    '#AAAAFF',
+    '#BBBBFF',
+    '#CCCCFF',
+    '#DDDDFF',
+    '#EEEEFF',
     '#FFFFFF'
 ];
 
