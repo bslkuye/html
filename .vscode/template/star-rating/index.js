24976bd85917c8b07d2d73b095d91160cea213c9
bslkuye <jdh960401@naver.com>
2022-12-19 18:25:02 +0900

    prac

--- .vscode/template/star-rating/index.js
+++ .vscode/template/star-rating/index.js
@@ -1,1 +1,20 @@
 // do something!
+const StarRating = $container => {
+  let star = null;
+  for(let i = 0; i < $container.getAttribute('data-max-rating'); i++){
+    star = document.createElement('i');
+    star.src = './star.svg';
+    star.id = 'star' + (i + 1);
+    // star.className = 'none';
+    $container.appendChild(star);
+  }
+  const simg = document.querySelectorAll('img');
+  console.log(simg)
+  // simg[i].addEventListener('onclick', () => {
+  //   console.log(i);
+  // })
+
+
+}
+
+export default StarRating;
