daf7c75a33a1649cf10dee407404162e3e162e30
bslkuye <jdh960401@naver.com>
2023-01-12 23:08:44 +0900

    pred

--- .vscode/programmers/lv3/다리를 지나는 트럭/main.js
+++ .vscode/programmers/lv3/다리를 지나는 트럭/main.js
@@ -2,8 +2,7 @@ function solution(bridge_length, weight, truck_weights) {
   var answer = 0;
   let sum = 0;
   let bridge = Array.from({length: bridge_length}, () => 0);
-  while(truck_weights.length > 0 && answer += 1){
-      // answer++;
+  while(truck_weights.length > 0){
       sum -= bridge.shift();
       if(sum + truck_weights[0] <= weight){
           sum += truck_weights[0];
