a69942d41a6f62e822669282f3e9245fdeab3860
bslkuye <jdh960401@naver.com>
2023-01-19 18:00:32 +0900

    prec

--- .vscode/materialsPrice/app.js
+++ .vscode/materialsPrice/app.js
@@ -1,9 +1,89 @@
 fetch("./data.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
+  .then(response => {
+    return response.json();
+  })
+  .then(jsondata => console.log(jsondata));
 
 //지도로 표시, 카테고리로 선택, 선택한 항목들 그래프란에 표시, 환율에 따라 계산해서 그래프 단위 통일
 //추세선, 같은 원료, 선택한(전체)원료 가격변동 추이, 가격변동 정도는 %로 계산
 //그래프는 마우스 오버 시 해당 칸의 정보 표시, 클릭하면 유지 (여러 그래프가 겹쳐있으므로 따로 원자재 하나 선택 후 마우스 오버)
+
+// google.charts.load('current', {packages: ['corechart', 'line']});
+// google.charts.setOnLoadCallback(drawTrendlines);
+
+// function drawTrendlines() {
+//       var data = new google.visualization.DataTable();
+//       data.addColumn('number', 'X');
+//       data.addColumn('number', 'Dogs');
+//       data.addColumn('number', 'Cats');
+
+//       data.addRows([
+//         [0, 0, 0],    [1, 10, 0],   [2, 23, 15],  [3, 17, 9],   [4, 18, 10],  [5, 9, 5],
+//         [6, 11, 3],   [7, 27, 19],  [8, 33, 25],  [9, 40, 32],  [10, 32, 24], [11, 35, 27],
+//         [12, 30, 22], [13, 40, 32], [14, 42, 34], [15, 47, 39], [16, 44, 36], [17, 48, 40],
+//         [18, 52, 44], [19, 54, 46], [20, 42, 34], [21, 55, 47], [22, 56, 48], [23, 57, 49],
+//         [24, 60, 52], [25, 50, 42], [26, 52, 44], [27, 51, 43], [28, 49, 41], [29, 53, 45],
+//         [30, 55, 47], [31, 60, 52], [32, 61, 53], [33, 59, 51], [34, 62, 54], [35, 65, 57],
+//         [36, 62, 54], [37, 58, 50], [38, 55, 47], [39, 61, 53], [40, 64, 56], [41, 65, 57],
+//         [42, 63, 55], [43, 66, 58], [44, 67, 59], [45, 69, 61], [46, 69, 61], [47, 70, 62],
+//         [48, 72, 64], [49, 68, 60], [50, 66, 58], [51, 65, 57], [52, 67, 59], [53, 70, 62],
+//         [54, 71, 63], [55, 72, 64], [56, 73, 65], [57, 75, 67], [58, 70, 62], [59, 68, 60],
+//         [60, 64, 56], [61, 60, 52], [62, 65, 57], [63, 67, 59], [64, 68, 60], [65, 69, 61],
+//         [66, 70, 62], [67, 72, 64], [68, 75, 67], [69, 80, 72]
+//       ]);
+
+//       var options = {
+//         hAxis: {
+//           title: 'Time'
+//         },
+//         vAxis: {
+//           title: 'Popularity'
+//         },
+//         colors: ['#AB0D06', '#007329'],
+//         trendlines: {
+//           0: {type: 'exponential', color: '#333', opacity: 1},
+//           1: {type: 'linear', color: '#111', opacity: .3}
+//         }
+//       };
+
+//       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+//       chart.draw(data, options);
+//     }
+google.charts.load('current', { 'packages': ['corechart', 'line'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses', 'aa'],
+    ["2023/01/17", 82.54, 82.54 + 1, 21],
+    ["2023/01/16", 81.79, 81.79 + 1, 21],
+    ["2023/01/13", 81.07, 81.07 + 1, 21],
+    ["2023/01/12", 79.53, 79.53 + 1, 21],
+    ["2023/01/11", 76.84, 76.84 + 1, 21],
+    ["2023/01/10", 76.04, 76.04 + 1, 21],
+    ["2023/01/09", 77.09, 77.09 + 1, 21],
+    ["2023/01/06", 75.20, 75.20 + 1, 21],
+    ["2023/01/05", 75.19, 75.19 + 1, 21],
+    ["2023/01/04", 77.10, 77.10 + 1, 21],
+    ["2023/01/03", 82.07, 82.07 + 1, 21],
+    ["2022/12/30", 78.77, 78.77 + 1, 21],
+    ["2022/12/29", 78.15, 78.15 + 1, 21],
+    ["2022/12/28", 80.00, 80.00 + 1, 21],
+    ["2022/12/27", 80.63, 80.63 + 1, 21],
+    ["2022/12/23", 78.11, 78.11 + 1, 21],
+    ["2022/12/22", 78.88, 78.88 + 1, 21],
+    ["2022/12/21", 76.74, 76.74 + 1, 21],
+    ["2022/12/20", 75.79, 75.79 + 1, 21],
+    ["2022/12/19", 75.36, 75.36 + 1, 21]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
--- .vscode/spaceWalk/backgroundImg.js
+++ .vscode/spaceWalk/backgroundImg.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector(".canvas");
+const space = document.querySelector('.space')
 const astro = document.getElementsByClassName("characters");
 const ctx = canvas.getContext("2d");
 
@@ -11,15 +12,25 @@ canvas.width = leng;
 canvas.height = leng;
 
 function touchCheck(){
-  if(x_position < -2450 && x_position > 450) x_speed *= -1;
-  if(y_position < -2450 && y_position > 450) y_speed *= -1;
+  if(x_position < -2450 || x_position > 450) x_speed *= -1;
+  if(y_position < -2450 || y_position > 450) y_speed *= -1;
 }
 
-function paintCell(x, y, alpha) {
+function paintCell(x, y, color, alpha) {
   ctx.beginPath();
   ctx.globalAlpha = alpha;
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = color;
   ctx.rect(x, y, 1, 1);
+  ctx.fill();
+}
+
+function paintCellBig(x, y, color, alpha) {
+  ctx.beginPath();
+  ctx.globalAlpha = alpha;
+  ctx.fillStyle = color;
+  ctx.rect(x - 2, y, 5, 1);
+  ctx.fill();
+  ctx.rect(x, y - 2, 1, 5);
   ctx.fill();
 }
 
@@ -31,11 +42,15 @@ function getRandomInt(min, max) {
 
 const makeStar = setInterval(() => {
   getRandomInt(0, leng, Math.random());
-  paintCell(getRandomInt(0, leng), getRandomInt(0, leng));
+  
+  paintCellBig(getRandomInt(0, leng), getRandomInt(0, leng), 'white');
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), 'white');
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), 'rgb(255, 100, 100)');
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), 'rgb(0, 150, 255)');
   astroSpin();
-}, 10);
+}, 1);
 
-let spin = 1;
+let spin = 0.1;
 let deg = 0;
 
 function astroSpin() {
@@ -43,21 +58,31 @@ function astroSpin() {
   deg = (deg + spin) % 360;
 }
 
-let x_speed = 0.1;
-let y_speed = 1;
+let x_speed = 0.2;
+let y_speed = 0.1;
 
 setTimeout(() => {
   clearTimeout(makeStar);
   setInterval(() => {
     x_position += x_speed;
     y_position += y_speed;
-    canvas.style.setProperty('--x-position', x_position + 'px');
-    canvas.style.setProperty('--y-position', y_position + 'px');
+    space.style.setProperty('--x-position', x_position + 'px');
+    space.style.setProperty('--y-position', y_position + 'px');
     astroSpin();
     touchCheck();
   }, 10);
-}, 10000);
+}, 1000);
 
+/**
+item - 
+배경 별 : 색, 크기, 개수, 특별한 모양
+배경 obj : 우주선, 우주인, 작은 행성, 성운
+obj : 사진 직접 추가로 날아다니게 , 크기
 
+무한맵 생성하는 방법 구상
+ㅁㅁㅁ
+ㅁㅁㅁ
+ㅁㅁㅁ
+위와 같이 같은 맵을 9개 복사해서 붙이고 가운데 맵을 벗어나면 가운데 맵의 반대편으로 이동시키기
 
-
+ */
