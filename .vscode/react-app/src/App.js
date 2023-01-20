da447c3df91ce14cc547d1e3d177652d2f871f5c
bslkuye <jdh960401@naver.com>
2023-01-20 22:29:21 +0900

    prec

--- .vscode/react-app/src/App.js
+++ .vscode/react-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from "./logo.svg"
 import "./App.css"
+import React, { useEffect, useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  //useEffect hook 사용해보기
+  /**
+ mount 리액트 컴포넌트가 그려질 때 랜더 될 때
+ unmount리액트 컴포넌트가 사라질 때 지워질 때
+ update 특정 값이 변해서 래액트 컴포넌트가 다시 그려질 때
+ */
+  const [count, setCount] = useState(0)
+
+  useEffect(() => console.log("hello"), [count]) // count가 변할때마다 호출됨 update
+  useEffect(() => console.log("hello"), []) // mount
+  useEffect(() => console.log("hello")) // unmount
+
+  //lifecycle 생명주기 메소드
+  /**
+   * dom이 생성, 업데이트, 제거될 때 어떤 행동을 할지 결정하는 메소드
+   */
+
+  //hook 사용 규칙
+
+  return <div className="App"></div>
 }
 
 export default App
