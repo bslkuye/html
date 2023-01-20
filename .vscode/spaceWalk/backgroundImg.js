0e9a35a6143ce4bb435eae3ffbaa4b2a5d70988b
bslkuye <jdh960401@naver.com>
2023-01-21 02:27:40 +0900

    prec

--- .vscode/spaceWalk/backgroundImg.js
+++ .vscode/spaceWalk/backgroundImg.js
@@ -1,47 +1,61 @@
-const canvas = document.querySelector(".canvas");
+const canvas = document.querySelectorAll(".map");
 const space = document.querySelector(".space");
 const astro = document.getElementsByClassName("characters");
-const ctx = canvas.getContext("2d");
+const ctx = canvas[0].getContext("2d");
 
 const leng = 3000;
 
-let x_position = 0;
-let y_position = 0;
+let x_position = -leng;
+let y_position = -leng;
 
-canvas.width = leng;
-canvas.height = leng;
+for (let i = 0; i < 9; i++) {
+  console.log(i);
+  canvas[i].width = leng;
+  canvas[i].height = leng;
+}
 
 function touchCheck() {
-  if (x_position < -2450 || x_position > 450) x_speed *= -1;
-  if (y_position < -2450 || y_position > 450) y_speed *= -1;
+  if (x_position > -1500) x_position -= 3000;
+  if (x_position < -4500) x_position += 3000;
+  if (y_position > -1500) y_position -= 3000;
+  if (y_position < -4500) y_position += 3000;
 }
 
 function paintCell(x, y, color, alpha) {
-  ctx.beginPath();
-  ctx.globalAlpha = alpha;
-  ctx.fillStyle = color;
-  ctx.rect(x, y, 1, 1);
-  ctx.fill();
+  for (let i = 0; i < 9; i++) {
+    const ctx = canvas[i].getContext("2d");
+    ctx.beginPath();
+    ctx.globalAlpha = alpha;
+    ctx.fillStyle = color;
+    ctx.rect(x, y, 1, 1);
+    ctx.fill();
+  }
 }
 
 function paintCellBig(x, y, color, alpha) {
-  ctx.beginPath();
-  ctx.globalAlpha = alpha;
-  ctx.fillStyle = color;
-  ctx.rect(x - 2, y, 5, 1);
-  ctx.fill();
-  ctx.rect(x, y - 2, 1, 5);
-  ctx.fill();
+  for (let i = 0; i < 9; i++) {
+    const ctx = canvas[i].getContext("2d");
+    ctx.beginPath();
+    ctx.globalAlpha = alpha;
+    ctx.fillStyle = color;
+    ctx.rect(x - 2, y, 5, 1);
+    ctx.fill();
+    ctx.rect(x, y - 2, 1, 5);
+    ctx.fill();
+  }
 }
 
 function paintCellMiddle(x, y, color, alpha) {
-  ctx.beginPath();
-  ctx.globalAlpha = alpha;
-  ctx.fillStyle = color;
-  ctx.rect(x - 1, y, 3, 1);
-  ctx.fill();
-  ctx.rect(x, y - 1, 1, 3);
-  ctx.fill();
+  for (let i = 0; i < 9; i++) {
+    const ctx = canvas[i].getContext("2d");
+    ctx.beginPath();
+    ctx.globalAlpha = alpha;
+    ctx.fillStyle = color;
+    ctx.rect(x - 1, y, 3, 1);
+    ctx.fill();
+    ctx.rect(x, y - 1, 1, 3);
+    ctx.fill();
+  }
 }
 
 function getRandomInt(min, max) {
@@ -59,7 +73,11 @@ const makeStar = setInterval(() => {
     getRandomInt(2, leng - 2),
     "rgb(0, 150, 255)"
   );
-  paintCellMiddle(getRandomInt(0, leng), getRandomInt(0, leng), "white");
+  paintCellMiddle(
+    getRandomInt(1, leng - 1),
+    getRandomInt(1, leng - 1),
+    "white"
+  );
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "white");
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(255, 100, 100)");
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(0, 150, 255)");
@@ -86,7 +104,7 @@ setTimeout(() => {
     space.style.setProperty("--y-position", y_position + "px");
     astroSpin();
     touchCheck();
-  }, 10);
+  }, 16);
 }, 1000);
 
 /**
@@ -94,11 +112,5 @@ item -
 배경 별 : 색, 크기, 개수, 특별한 모양
 배경 obj : 우주선, 우주인, 작은 행성, 성운
 obj : 사진 직접 추가로 날아다니게 , 크기
-
-무한맵 생성하는 방법 구상
-ㅁㅁㅁ
-ㅁㅁㅁ
-ㅁㅁㅁ
-위와 같이 같은 맵을 9개 복사해서 붙이고 가운데 맵을 벗어나면 가운데 맵의 반대편으로 이동시키기
 
  */
