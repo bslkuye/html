382a2129fd8eae0b0062189b1a42b9b7e384b049
bslkuye <jdh960401@naver.com>
2023-01-21 20:07:48 +0900

    prec

--- .vscode/spaceWalk/backgroundImg.js
+++ .vscode/spaceWalk/backgroundImg.js
@@ -1,9 +1,17 @@
 const canvas = document.querySelectorAll(".map");
 const space = document.querySelector(".space");
-const astro = document.getElementsByClassName("characters");
+const astro = document.querySelector(".characters");
 const ctx = canvas[0].getContext("2d");
+const obj = document.querySelectorAll(".object");
 
-const leng = 3000;
+const leng = 2000;
+document.documentElement.style.setProperty("--width", leng + "px");
+
+/** [x_position, y_position, x_speed, y_speed, spin, deg] */
+let obj_info = [
+  [leng / 2, leng / 2, 0.2, 0.1, 0.1, 0],
+  [0, 0, 1, 2, 1, 0],
+];
 
 let x_position = -leng;
 let y_position = -leng;
@@ -14,11 +22,12 @@ for (let i = 0; i < 9; i++) {
   canvas[i].height = leng;
 }
 
-function touchCheck() {
-  if (x_position > -1500) x_position -= 3000;
-  if (x_position < -4500) x_position += 3000;
-  if (y_position > -1500) y_position -= 3000;
-  if (y_position < -4500) y_position += 3000;
+function touchCheck(info_arr) {
+  [x, y] = info_arr.slice(0, 2);
+  if (x_position > -1 * leng * 0.5) x_position -= leng;
+  if (x_position < -1 * leng * 1.5) x_position += leng;
+  if (y_position > -1 * leng * 0.5) y_position -= leng;
+  if (y_position < -1 * leng * 1.5) y_position += leng;
 }
 
 function paintCell(x, y, color, alpha) {
@@ -81,28 +90,37 @@ const makeStar = setInterval(() => {
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "white");
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(255, 100, 100)");
   paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(0, 150, 255)");
-  astroSpin();
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "white");
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(255, 100, 100)");
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(0, 150, 255)");
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "white");
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(255, 100, 100)");
+  paintCell(getRandomInt(0, leng), getRandomInt(0, leng), "rgb(0, 150, 255)");
 }, 1);
 
 let spin = 0.1;
 let deg = 0;
 
 function astroSpin() {
-  document.documentElement.style.setProperty("--deg", deg + spin);
+  astro.style.setProperty("--deg", deg);
   deg = (deg + spin) % 360;
 }
 
 let x_speed = 0.2;
 let y_speed = 0.1;
 
+function astroMove() {
+  x_position += x_speed;
+  y_position += y_speed;
+  space.style.setProperty("--x-position", x_position + "px");
+  space.style.setProperty("--y-position", y_position + "px");
+  astroSpin();
+}
+
 setTimeout(() => {
   clearTimeout(makeStar);
   setInterval(() => {
-    x_position += x_speed;
-    y_position += y_speed;
-    space.style.setProperty("--x-position", x_position + "px");
-    space.style.setProperty("--y-position", y_position + "px");
-    astroSpin();
+    astroMove();
     touchCheck();
   }, 16);
 }, 1000);
@@ -112,5 +130,8 @@ item -
 배경 별 : 색, 크기, 개수, 특별한 모양
 배경 obj : 우주선, 우주인, 작은 행성, 성운
 obj : 사진 직접 추가로 날아다니게 , 크기
+
+
+캔버스마다 오브젝트를 넣어서 포지션을 복잡하게 안해도 되게
 
  */
