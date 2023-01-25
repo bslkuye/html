65e3bf9ec47f2ab1870027c566586350697f3dd4
bslkuye <jdh960401@naver.com>
2023-01-25 17:40:12 +0900

    prec

--- .vscode/react-app/src/App.js
+++ .vscode/react-app/src/App.js
@@ -1,7 +1,22 @@
 import styles from "./App.module.css";
+import Button from "./components/Button";
+import Header from "./Header";
 
 function App() {
-  return <div className={styles.nav}>nav</div>;
+  return (
+    <>
+      <div className={styles.nav}>Nav</div>
+      <Header />
+      <div className={styles.listContainer}>
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
+        >
+          New issue
+        </Button>
+      </div>
+      <div className={styles.footer}>footer</div>
+    </>
+  );
 }
 
 export default App;
