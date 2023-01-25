911c88546b8933b9b8f0bfbfcb342ad6bc6bc43b
bslkuye <jdh960401@naver.com>
2023-01-26 03:18:21 +0900

    prec

--- .vscode/react-app/src/App.js
+++ .vscode/react-app/src/App.js
@@ -1,19 +1,15 @@
 import styles from "./App.module.css";
+
 import Button from "./components/Button";
 import Header from "./Header";
+import ListContainer from "./ListContainer";
 
 function App() {
   return (
     <>
       <div className={styles.nav}>Nav</div>
       <Header />
-      <div className={styles.listContainer}>
-        <Button
-          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
-        >
-          New issue
-        </Button>
-      </div>
+      <ListContainer />
       <div className={styles.footer}>footer</div>
     </>
   );
---
+++ .vscode/react-app/src/ListContainer.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import Button from "./components/Button";
+import styles from "./ListContainer.module.css";
+
+export default function ListContainer() {
+  const [inputValue, setInputValue] = useState("is:pr is:open");
+
+  useEffect(() => {
+    // console.log({ inputValue });
+  }, [inputValue]);
+  return (
+    <div className={styles.listContainer}>
+      <div className={styles.topSection}>
+        <input
+          className={styles.input}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
+        >
+          New Issue
+        </Button>
+      </div>
+    </div>
+  );
+}
