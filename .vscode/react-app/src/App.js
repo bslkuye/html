0a6802f2f229b8dd516bc4c0e23d64648ae650d6
bslkuye <jdh960401@naver.com>
2023-01-29 19:35:15 +0900

    prec

--- .vscode/react-app/src/App.js
+++ .vscode/react-app/src/App.js
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import axios from "axios";
 
 import Button from "./components/Button";
 import Header from "./Header";
@@ -6,6 +7,14 @@ import ListContainer from "./ListContainer";
 import Footer from "./Footer";
 
 function App() {
+  async function getData() {
+    const data = await axios.get(
+      `https://api.github.com/repos/facebook/react/issues`,
+    );
+  }
+
+  getData();
+
   return (
     <>
       <div className={styles.nav}>Nav</div>
