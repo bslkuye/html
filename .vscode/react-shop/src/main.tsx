15a311caf673952a72a1038c15041555d820d99d
bslkuye <jdh960401@naver.com>
2023-02-01 20:13:27 +0900

    prec

--- .vscode/react-shop/src/main.tsx
+++ .vscode/react-shop/src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('aaa')
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <div>
+      aaa
+    </div>
   </React.StrictMode>,
 )
