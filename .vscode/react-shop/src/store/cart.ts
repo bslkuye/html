8a672cdf96e530485b6d9db5bef4c54741ed9edc
bslkuye <jdh960401@naver.com>
2023-02-07 16:24:48 +0900

    prec

--- .vscode/react-shop/src/store/cart.ts
+++ .vscode/react-shop/src/store/cart.ts
@@ -81,7 +81,7 @@ export const addToCart = (cart: CartState, id: number) => {
   return { ...cart, [id]: { id: { id, count: cartState[id].count }}};
 };
 
-export const removeFromCart = (cart: cartState, id: number) => {
+export const removeFromCart = (cart: CartState, id: number) => {
   const tempCart = { ...cart };
   if(tempCart[id].count === 1){
     delete tempCart[id];
