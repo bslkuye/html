e4b075d9ac05942a67f275796bc66e4717b5e929
bslkuye <jdh960401@naver.com>
2023-02-12 12:56:03 +0900

    prec

--- .vscode/react-shop/src/main.tsx
+++ .vscode/react-shop/src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import createRoot from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
 import { CART_ITEM } from './composables/useCartLoad';
 import { cartState } from './store/cart';
 import App from './App';
 
-const container: any = document.getElementById('app')
-const root = createRoot(container);
+// const container: any = document.getElementById('app')
+// const root = createRoot(container);
 const initialValue = JSON.parse(localStorage.getItem(CART_ITEM) as string) ?? {};
+
+const root = ReactDOM.createRoot(
+  document.getElementById('app') as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
