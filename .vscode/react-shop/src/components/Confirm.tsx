13cc84ee8f15f3ef6ec868c3ea9d29e4e32147e7
bslkuye <jdh960401@naver.com>
2023-02-15 23:16:26 +0900

    prec

--- .vscode/react-shop/src/components/Confirm.tsx
+++ .vscode/react-shop/src/components/Confirm.tsx
@@ -1,14 +1,14 @@
 import { useRecoilState } from "recoil";
 import { CartState, cartState } from '../store/cart';
-
+import styles from './Confirm.module.css'
 const Confirm = (): JSX.Element => {
   const [cart, setCart] = useRecoilState<CartState>(cartState);
   const buyItems = () => setCart({} as CartState);
   return(
     <>
-      <input type='checkbox' id='confirm-modal' className='modal-toggle' />
-      <div className="modal">
-        <div className="modal-box">
+      <input type='checkbox' id='confirm-modal' className={styles.modalToggle} />
+      <div className={styles.modal}>
+        <div className={styles.modalBox}>
           <h3 className="font-bold text-lg">정말로 구매하시겠습니까?</h3>
           <p className="py-4">장바구니의 모든 상품들이 삭제됩니다.</p>
           <div className="modal-action">
--- .vscode/react-shop/src/components/Drower.tsx
+++ .vscode/react-shop/src/components/Drower.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from './Drower.module.css'
 
 const Drawer = ({ closeOverlay }: any): JSX.Element => {
   const menus = [
@@ -8,8 +9,8 @@ const Drawer = ({ closeOverlay }: any): JSX.Element => {
   ];
 
   return(
-    <div className="drawer-side">
-      <label htmlFor="side-menu" className="drawer-overlay"></label>
+    <div className={styles.drawerSide}>
+      <label htmlFor="side-menu" className={styles.drawerOverlay}></label>
       <ul className="menu w-60 sm:w-80 p-4 overflow-y-auto bg-white dark:bg-base-100">
         {menus.map((menu) => {
           return(
