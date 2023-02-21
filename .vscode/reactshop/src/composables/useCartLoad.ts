9a2d757cf5592dda6263e018e6b27e7353bab93d
bslkuye <jdh960401@naver.com>
2023-02-21 19:54:41 +0900

    prec

---
+++ .vscode/reactshop/src/composables/useCartLoad.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { cartState } from '../store/cart';
+
+export const CART_ITEM = 'CART_ITEM';
+
+export const useCartLoad = () => {
+  const cartStore = useRecoilValue(cartState);
+  const setCartData = () => {
+    localStorage.setItem(CART_ITEM, JSON.stringify(cartStore));
+  };
+  useEffect(() => {
+    return () => {
+        setCartData()
+    }
+  }, [cartStore]);
+};
