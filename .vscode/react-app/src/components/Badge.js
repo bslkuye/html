4ab6bd63d0edbdba83278946a4ac3ae8e09851de
bslkuye <jdh960401@naver.com>
2023-02-25 03:14:55 +0900

    prec

--- .vscode/react-app/src/components/Badge.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Badge.module.css";
-
-export default function Badge({ name, color }) {
-  return (
-    <span className={styles.badge} style={{ background: `#${color}` }}>
-      {name}
-    </span>
-  );
-}
